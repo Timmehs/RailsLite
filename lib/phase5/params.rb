320b3bf9210f2620cdea97214c26cdff602bdc7d
Tim Sandberg <tasandberg@gmail.com>
2014-11-05 14:50:10 -0800

    Pass all specs, troubleshooting server:

--- lib/phase5/params.rb
+++ lib/phase5/params.rb
@@ -12,19 +12,9 @@ module Phase5
 
     def initialize(req, route_params = {})
       @params = route_params
-      if req.query_string
-        query_string_hash = {}
-        query = URI::decode_www_form(req.query_string)
-        query.each do |key, val|
-          # check if
-          if nested_key?(key)
-
-          else
-            query_string_hash[key] = val
-          end
-        end
-        @params.merge!(query_string_hash)
-      end
+      @params
+        .merge!(parse_www_encoded_form(req.query_string)) if req.query_string
+      @params.merge!(parse_www_encoded_form(req.body)) if req.body
     end
 
     def nested_key?(key)
@@ -44,37 +34,36 @@ module Phase5
     private
     # this should return deeply nested hash
     # argument format
-    # user[address][street]=main&user[address][zip]=89436
+    # parse_www_encoded_form("user[address][street]=main&user[address][zip]=89436")
     # should return
     # { "user" => { "address" => { "street" => "main", "zip" => "89436" } } }
 
     def parse_www_encoded_form(www_encoded_form)
       key_vals = URI::decode_www_form(www_encoded_form)
-      hashes = []
-      key_vals.each do |key_set, val|
-        hash = {}
-        parsed_keys = parse_key(key_set).reverse
 
-
-        parsed_keys.each_with_index do |key, i|
-          if i == 0
-            hash[key] = val
-          else
-            hash = { key => { parsed_keys[i - 1] => hash[parsed_keys[i - 1]] } }
-          end
-        end
-        hashes << hash
-      end
-      hashes
+      render_www_to_hash(key_vals)
     end
 
+    def render_www_to_hash(key_value_sets)
+      params = {}
+      key_value_sets.each do |key_string, val|
+        key_set = parse_key(key_string)
+        current = params
+
+        key_set[0..-2].each do |key|
+          current[key] ||= {}
+          current = current[key]
+        end
+        current[key_set.last] = val
+      end
+
+      params
+    end
 
     # this should return an array
     # user[address][street] should return ['user', 'address', 'street']
     def parse_key(key)
       key.split(/\]\[|\[|\]/)
     end
-
-
   end
 end
